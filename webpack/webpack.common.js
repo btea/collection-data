19992d63ad0083e45a46b9ae29c191fe50b36a84
btea <2356281422@qq.com>
2021-07-11 17:14:57 +0800

    chore: 补充说明

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -162,7 +162,8 @@ module.exports = {
          * 注：网址后面应带上 '/' 否则插件会报错  Invalid value for 'content_scripts[0].matches[0]': Empty path.
          */
         new ReloadPlugin({
-            url: '<all_urls>' // 匹配所有地址
+            // <all_urls> 匹配所有地址
+            url: 'https://www.bilibili.com/'
         }),
         // new webpack.NamedModulesPlugin(),
         // new webpack.HotModuleReplacementPlugin(),
